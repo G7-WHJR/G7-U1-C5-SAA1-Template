1a95893449e116da25ae139d1d5b2f33a1652dae
G7-WHJR <raveen.anand@whitehatjr.com>
2021-06-02 18:53:13 +0530

    First

---
+++ Age Checker/myscript.js
@@ -0,0 +1,10 @@
+function validate()
+{
+    //1. Take input from age input field , store it in a variable
+    
+
+    //2. if age is lesser than 18, display the 'errorAge' span, else hide it
+    
+
+}
+
